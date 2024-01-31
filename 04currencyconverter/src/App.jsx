92d9d42ef00a434fc84f4d73ca57a9fc32af2033
sumukhanidhi <sumukhanidhi@gmail.com>
2024-01-31 12:24:30 +0530

    j

--- 04currencyconverter/src/App.jsx
+++ 04currencyconverter/src/App.jsx
@@ -4,8 +4,9 @@ function App() {
 
   return (
     <>
-      <div className="flex h-screen items-center justify-center bg-zinc-600 text-sky-500">
-        <h1 className='text-2xl uppercase hover:bg-slate-500 hover:rounded-lg  hover:text-sky-800 p-4 '>Hello There!!</h1>
+      <div className="flex h-screen flex-col items-center justify-center bg-zinc-600 text-sky-500">
+        <h1 className='text-2xl uppercase  border-teal-400 border-4 hover:bg-slate-500 hover:rounded-lg  hover:text-sky-800 p-4 m-2 '>Hello There!!</h1>
+         <h1 className='text-2xl uppercase border-teal-400 border-4  duration-300 hover:bg-slate-500 hover:rounded-full  hover:text-sky-800 p-4 m-2 '>Hello There!!</h1>
       </div>
     </>
   )
